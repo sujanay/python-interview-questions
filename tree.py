ffe080354739958a8516b734d7b4e405d553a4ca
Sujanay <sujanay@hotmail.com>
2019-02-12 12:02:50 -0500

    all three tree traversal included

--- tree.py
+++ tree.py
@@ -1,33 +1,57 @@
 from __future__ import print_function
 
+# Binary Tree Node
 class TreeNode:
     def __init__(self, v, l=None, r=None):
         self.value = v
         self.left = l
         self.right = r
 
+# insert data to the tree
 def tree_insert(root, x):
     attr = 'left' if x < root.value else 'right'
     side = getattr(root, attr)
     if not side: setattr(root, attr, TreeNode(x))
     else: tree_insert(side, x)
 
-def print_tree(root):
+# preorder traversal through the tree
+def print_tree_preorder(root):
     if root is None:
         return
     print(root.value, end=' - ')
-    print_tree(root.left)
-    print_tree(root.right)
+    print_tree_preorder(root.left)
+    print_tree_preorder(root.right)
 
-if __name__ == '__main__':
-    root = TreeNode(7)
+# inorder traversal through the tree
+def print_tree_inorder(root):
+    if root is None:
+        return
+    print_tree_inorder(root.left)
+    print(root.value, end=' - ')
+    print_tree_inorder(root.right)
 
-    tree_insert(root, 4)
-    tree_insert(root, 5)
-    tree_insert(root, 8)
-    tree_insert(root, 9)
-    tree_insert(root, 2)
-    tree_insert(root, 3)
+# postorder traversal through the tree
+def print_tree_postorder(root):
+    if root is None:
+        return
+    print_tree_postorder(root.left)
+    print_tree_postorder(root.right)
+    print(root.value, end = ' - ')
 
-    print_tree(root) # outputs: 7 - 4 - 2 - 3 - 5 - 8 - 9 -
+if __name__ == '__main__':          #            Tree Structure
+    root = TreeNode(7)              #                 7
+    tree_insert(root, 4)            #               /    \
+    tree_insert(root, 5)            #             4       8
+    tree_insert(root, 8)            #          /    \       \
+    tree_insert(root, 9)            #         2      5       9
+    tree_insert(root, 2)            #          \
+    tree_insert(root, 3)            #           3
+
+    print("Inorder traversal")
+    print_tree_inorder(root)        # outputs: 7 - 4 - 2 - 3 - 5 - 8 - 9 -
+   
+    print("\n\nPreorder traversal")
+    print_tree_preorder(root)       # outputs: 7 - 4 - 2 - 3 - 5 - 8 - 9 -
     
+    print("\n\nPostorder traversal")
+    print_tree_postorder(root)      # outputs: 3 - 2 - 5 - 4 - 9 - 8 - 7 -
