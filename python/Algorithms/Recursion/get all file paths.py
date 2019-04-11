387422a137d0d6e5e0343e10e2918008fce962b2
Sujanay <sujanay@hotmail.com>
2019-04-10 22:41:18 -0400

    Update get all file paths.py

--- python/Algorithms/Recursion/get all file paths.py
+++ python/Algorithms/Recursion/get all file paths.py
@@ -1,38 +1,36 @@
 import os
 
-
 # change the directory when a path is supplied
 # similar to ~cd command
 def changeDir(path):
-    new_dir = os.chdir(path)
-    return new_dir
-
+#     new_dir = os.chdir(path)
+#     return new_dir
+    pass
 
 # should return True / False
 def isDir(path):
     """ returns 'True' if the given path is a Directory """
-
     return os.path.isdir(path)
-
 
 # list all files in the given folder path
 # similar to ~ls command
 def listFiles(path):
     """ Return a list containing the names of the entries in the directory given by path """
-
     return os.listdir(path)
 
 
 # func getallpath(fileName):
 # return list containg all the path of the files
+
+all_files_list = []
+
 def getAllPaths(start_path, filename):
     """ returns all the file paths that matches the file 'filename' """
-    all_files_list = []
-
+    #     all_files_list = []
     files = listFiles(start_path)
     for file_name in files:
         full_link = start_path + '/' + file_name
-
+        
         if isDir(full_link):
             getAllPaths(full_link, filename)
         else:
@@ -40,7 +38,9 @@ def getAllPaths(start_path, filename):
                 all_files_list.append(full_link)
     return all_files_list
 
-start_path = "S:/recursive practice/newfolder"
+# Put your path here
+start_path = "S:/recursive practice/newfolder" 
+# Put your file here you want to search (use "*.txt" / "*.png" / "*.exe" for WILDCARD search)
 filename = "sample.txt.txt"
 
-print(getAllPaths(start_path, filename))
+getAllPaths(start_path, filename)
